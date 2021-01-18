5966ad78156dff9a06d134e98cd098474612bf10
natasha998 <agaranaty@gmail.com>
2021-01-18 18:30:57 -0300

    Ejercicio finalizado

--- ejercicio10/js/tarjeta-js.js
+++ ejercicio10/js/tarjeta-js.js
@@ -1,4 +1,32 @@
 document.addEventListener("DOMContentLoaded",()=>{
     "use strict";
+    function sendData(event){
+        event.preventDefault();
+        let nameUser = document.querySelector("#name").value;
+        let lastNameUser = document.querySelector("#lastName").value;
+        let nameCard = document.querySelector("#nameCard");
+        let lastNameCard = document.querySelector("#lastNameCard");
+        let classCard = document.querySelector("#card-js");
+
+        classCard.classList.add("tarjeta");
+        nameCard.innerText = nameUser;
+        lastNameCard.innerHTML = lastNameUser;
+    }
     
+    document.querySelector("#send-js").addEventListener('click', sendData);
+
+    document.querySelector("#option1").addEventListener('click', ()=>{
+        let classCard = document.querySelector("#card-js");
+        classCard.classList.add("opcion1");
+    })
+
+    document.querySelector("#option2").addEventListener('click', ()=>{
+        let classCard = document.querySelector("#card-js");
+        classCard.classList.add("opcion2");
+    })
+
+    document.querySelector("#option3").addEventListener('click', ()=>{
+        let classCard = document.querySelector("#card-js");
+        classCard.classList.add("opcion3");
+    })
 })
